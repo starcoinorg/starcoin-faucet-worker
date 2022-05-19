ef71d5e4f0b091f592401beb583106d103bbe4da
wk3368 <wk3368@163.com>
2022-05-19 16:38:10 +0800

    Use winston for better logging

--- README.md
+++ README.md
@@ -12,12 +12,6 @@ export STARCOIN_FAUCET_WORKER_SENDERS=[{\"address\":xxx\"\",\"privateKey\":\"xxx
 # Install
 
 ```
-yarn install
-```
-
-or
-
-```
 npm install
 ```
 
@@ -27,6 +21,20 @@ npm install
 
 ```
 node index.js --senderIndex=0 --count=5
+```
+
+The output is:
+
+```
+2022-05-19T08:34:45.865Z [info]: Connected to MySQL Server!
+2022-05-19T08:34:45.865Z [info]: ---Job start---
+2022-05-19T08:34:45.870Z [info]: 2 records founded.
+2022-05-19T08:34:48.493Z [info]: transaction_hash: 0xd46c42e6d5d06909bb99cf4bf086682f1eece1ac20c6dacad6695c921fbda5f9
+2022-05-19T08:34:48.493Z [info]: sequence_number: 287
+2022-05-19T08:35:14.782Z [info]: status: Executed
+2022-05-19T08:35:14.782Z [info]: gas_used: 221925
+2022-05-19T08:35:14.782Z [info]: block_number: 4660095
+2022-05-19T08:35:14.782Z [info]: ---Job finished---
 ```
 
 2. In emergency conditions(eg. thousands of new records were added on the promotion day), we can batch handle them in parrael using multi senders.
--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const mysql = require('mysql');
 const minimist = require('minimist');
-const { encoding, utils, providers } = require('@starcoin/starcoin')
+const { utils, providers } = require('@starcoin/starcoin')
+const { createLogger, format, transports } = require("winston");
+const { combine, label, timestamp, printf } = format;
+
+const loggerFormat = printf(info => `${ info.timestamp } [${ info.level }]: ${ typeof info.message === 'object' ? JSON.stringify(info.message) : info.message }`);
+
+const logLevels = {
+    fatal: 0,
+    error: 1,
+    warn: 2,
+    info: 3,
+    debug: 4,
+    trace: 5,
+};
+
+const logger = createLogger({
+    level: "warn",
+    levels: logLevels,
+    format: combine(
+        timestamp(),
+        loggerFormat
+    ),
+    transports: [new transports.Console({ level: "info" })],
+});
 
 const MYSQL_HOST = process.env.STARCOIN_FAUCET_WORKER_MYSQL_HOST || ''
 const MYSQL_USER = process.env.STARCOIN_FAUCET_WORKER_MYSQL_USER || ''
@@ -14,7 +37,7 @@ const NETWORK_MAP = {
 }
 
 const doJob = () => {
-    console.log('Job start')
+    logger.info('---Job start---')
     const args = minimist(process.argv.slice(2))
 
     const senderIndex = args['senderIndex'] || 0
@@ -22,10 +45,10 @@ const doJob = () => {
     connection.query(`SELECT id, network, address, amount from faucet_address where status=0 and transfer_retry=0 LIMIT ${ limit }`, (err, rows) => {
         if (err) throw err;
         if (rows.length === 0) {
-            console.log('No rows to handle!')
+            logger.info('No rows to handle!')
             return;
         }
-        console.log(`${ rows.length } records founded.`)
+        logger.info(`${ rows.length } records founded.`)
         const ids = []
         const addresses = []
         const amounts = []
@@ -37,11 +60,9 @@ const doJob = () => {
             // amounts.push(row.amount * STC_SCALLING_FACTOR)
             amounts.push(0.01 * STC_SCALLING_FACTOR)
         });
-        // console.log({ ids, addresses, amounts })
 
         const sender = SENDERS[senderIndex]
         batchTransfer(network, sender, addresses, amounts)
-        console.log('Job finished')
     });
     connection.end();
 }
@@ -59,7 +80,7 @@ const batchTransfer = async (network, sender, addressArray, amountArray) => {
     const senderSequenceNumber = await provider.getSequenceNumber(address)
     const chainId = NETWORK_MAP[network];
     const nowSeconds = await provider.getNowSeconds();
-    // console.log({ senderSequenceNumber, nowSeconds })
+
     const rawUserTransaction = utils.tx.generateRawUserTransaction(
         address,
         scriptFunction,
@@ -70,24 +91,23 @@ const batchTransfer = async (network, sender, addressArray, amountArray) => {
         chainId,
     );
 
-    const rawUserTransactionHex = encoding.bcsEncode(rawUserTransaction)
-    // console.log({ rawUserTransactionHex })
-
     const signedUserTransactionHex = await utils.tx.signRawUserTransaction(
         privateKey,
         rawUserTransaction,
     );
 
     const txn = await provider.sendTransaction(signedUserTransactionHex);
-    // console.log({ txn });
-    console.log('transaction_hash:', txn.transaction_hash)
-    console.log('sequence_number', txn.raw_txn.sequence_number)
-    const txnInfo = await txn.wait(1);
-    // console.log({ txnInfo });
-    console.log('status:', txnInfo.status);
-    console.log('gas_used:', txnInfo.gas_used);
-    console.log('block_number:', txnInfo.block_number);
 
+    logger.info(`transaction_hash: ${ txn.transaction_hash }`)
+    logger.info(`sequence_number: ${ txn.raw_txn.sequence_number }`)
+
+    const txnInfo = await txn.wait(1);
+
+    logger.info(`status: ${ txnInfo.status }`)
+    logger.info(`gas_used: ${ txnInfo.gas_used }`)
+    logger.info(`block_number: ${ txnInfo.block_number }`)
+
+    logger.info('---Job finished---')
 }
 
 const connection = mysql.createConnection({
@@ -99,7 +119,7 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
     if (err) throw err;
-    console.log('Connected to MySQL Server!');
+    logger.info('Connected to MySQL Server!');
     doJob();
 });
 
